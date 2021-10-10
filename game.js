76bfa4bdb0d575ba7cd0d09b669f7638c9c3bf2d
ideens <ideens@users.noreply.github.com>
2021-10-10 22:10:01 +0100

    drew some test platforms on canvas

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const context =canvas.getContext('2d')
 let gameStarted = false
 const keys = []
 const friction = 0.8
+const gravity = 0.98
 
 const player = {
     x: 5,
@@ -13,12 +14,36 @@ const player = {
     speed: 5,
     velX: 0,
     velY: 0,
-    color: "#ff0000",
+    jumpStrength: 7,
+    jumping: false,
+    color: "#ff99a9",
     draw: function() {
         context.fillStyle = this.color
         context.fillRect(this.x,this.y,this.width, this.height)
     }
 }
+
+// PLATFORMS
+const platforms = []
+platforms.push({
+    x: 100,
+    y: 300,
+    width: 120,
+    height: 10,
+})
+platforms.push({
+    x: 240,
+    y: 260,
+    width: 120,
+    height: 10,
+})
+platforms.push({
+    x: 380,
+    y: 220,
+    width: 120,
+    height: 10,
+})
+
 
 // KEYS
 document.addEventListener ("keydown", function(e){
@@ -67,11 +92,29 @@ function clearCanvas() {
     context.clearRect(0,0,640,640)
 }
 
+// Function to draw platforms
+function drawPlatforms() {
+    context.fillStyle = "#3f754e"
+    for (let i = 0; i < platforms.length; i++) {
+        context.fillRect(platforms[i].x , platforms[i].y , platforms[i].width , platforms[i].height)
+    }
+
+}
+
+
 
 // Main game loop
 function gameLoop() {
     console.log("Game Running")
     player.draw()
+    drawPlatforms()
+    if(keys["ArrowUp"] || keys[" "]) {
+        console.log("Up Key or Space Pressed")
+        if(!player.jumping){
+            player.velY = -player.jumpStrength*1.5
+            player.jumping = true
+        }
+    }
     
     if(keys["ArrowRight"]) {
         console.log("Right Key Pressed")
@@ -91,5 +134,13 @@ function gameLoop() {
 
     player.x += player.velX
     player.velX *= friction //friction for player to stop on keyup
+    player.y += player.velY
+    player.velY += gravity //increase gravity when jumped
+
+    if(player.y >= canvas.height - player.height) {
+        player.y = canvas.height - player.height
+        player.jumping = false
+    }
+
 
 }
